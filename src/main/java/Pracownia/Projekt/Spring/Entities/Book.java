dd4c1b93b55c48470d97214a4939c41f72ecf0b0
Alicja Szulecka <73056579+AliSzu@users.noreply.github.com>
2023-10-28 17:39:40 +0200

    feat: patch delete book

--- src/main/java/Pracownia/Projekt/Spring/Entities/Book.java
+++ src/main/java/Pracownia/Projekt/Spring/Entities/Book.java
@@ -1,21 +1,29 @@
 package Pracownia.Projekt.Spring.Entities;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
-    private int id;
+    private Long id;
     @Column
-    private String name;
+    private String title;
     @Column
-    private String surname;
+    private String author;
+    @Column
+    @CreationTimestamp
+    private Date createdAt;
     public Book() {}
 
 }
--- src/main/java/Pracownia/Projekt/Spring/Services/BookService.java
+++ src/main/java/Pracownia/Projekt/Spring/Services/BookService.java
@@ -1,11 +1,17 @@
 package Pracownia.Projekt.Spring.Services;
 
+import Pracownia.Projekt.Spring.DTO.PatchBookDTO;
 import Pracownia.Projekt.Spring.Entities.Book;
 import Pracownia.Projekt.Spring.Repositories.BookRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.beans.FeatureDescriptor;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
 
 
 @Service
@@ -21,4 +27,35 @@ public class BookService {
     public Book createBook(Book book) {
         return bookRepository.save(book);
     }
+    public boolean existsById(Integer id) {
+        return bookRepository.existsById(id);
+    }
+
+    public void deleteById(Integer id) {
+        bookRepository.deleteById(id);
+    }
+
+    public Optional<Book> getBookById(Integer id) {return bookRepository.findById(id);}
+
+    public Book mergeBooks( Book aCurrentBook, Book aNewBook) {
+        return Book.builder()
+                .author(aNewBook.getAuthor() != null ? aNewBook.getAuthor() : aCurrentBook.getAuthor())
+                .title(aNewBook.getTitle() != null ? aNewBook.getTitle() : aCurrentBook.getTitle())
+                .build();
+    }
+
+    public Book patchBook(PatchBookDTO newBook, Integer id) {
+
+        final BeanWrapper wrappedSource = new BeanWrapperImpl(newBook);
+        String[] nulls = Stream.of(wrappedSource.getPropertyDescriptors())
+                .map(FeatureDescriptor::getName)
+                .filter(propertyName -> wrappedSource.getPropertyValue(propertyName) == null)
+                .toArray(String[]::new);
+
+        Book bookToUpdate = bookRepository.getReferenceById(id);
+        BeanUtils.copyProperties(newBook, bookToUpdate, nulls);
+
+        return bookRepository.save(bookToUpdate);
+    }
+
 }
